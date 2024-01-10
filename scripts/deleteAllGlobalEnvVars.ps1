621f1c63f2e42d5230d745f2582c04d592473241
paulober <44974737+paulober@users.noreply.github.com>
2024-01-10 20:08:02 +0100

    Remove utilities for legacy beta versions
    Signed-off-by: paulober <44974737+paulober@users.noreply.github.com>

--- scripts/deleteAllGlobalEnvVars.ps1
+++
@@ -1,6 +0,0 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$EnvVariablePrefix
-)
-
-Get-ChildItem "Env:$EnvVariablePrefix*" | ForEach-Object { [System.Environment]::SetEnvironmentVariable($_.Name, $null, [System.EnvironmentVariableTarget]::User) }
